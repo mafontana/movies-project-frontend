ab5dbab4cffad2a448c1a024b5a831ac8c6d9373
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-12-06 18:04:08 -0700

    created Background component and set background image

---
+++ src/components/Background.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Background extends Component {
+    render() {
+      return (
+        <div className="Background">
+        <h1>HIIIIIIIII </h1>
+        </div>
+      );
+    }
+  }
+  
+  export default Background;
