47164e9aa5c82f6a2dee4d3e7e8bb192f7d31a7b
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-12-12 17:18:27 -0700

    fixed formatting of home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Background from "./components/Background";
 import Home from "./components/Home";
 
 
